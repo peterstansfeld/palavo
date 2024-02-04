8679ee69a7cf66b5993d542a36cf2082daaf5519
Peter Stansfeld <pstansfeld@redcreations.co.uk>
2024-02-04 23:26:11 +0000

    Add logic analyser to sample hsync
    Text output only at the moment, via the uart.

--- vga_graphics_demo.c
+++ vga_graphics_demo.c
@@ -26,6 +26,7 @@
 #include "hardware/pio.h"
 #include "hardware/dma.h"
 #include "hardware/uart.h"
+// #include "hardware/structs/bus_ctrl.h"
 
 #define UART_ID uart1
 #define BAUD_RATE 115200
@@ -48,12 +49,101 @@ bool repeating_timer_callback(struct repeating_timer *t) {
 }
 
 
+const uint CAPTURE_PIN_BASE = 16; // 16 = hsync, 17 = vsync
+const uint CAPTURE_PIN_COUNT = 1;
+const uint CAPTURE_N_SAMPLES = 640; //was 96
+
+static inline uint bits_packed_per_word(uint pin_count) {
+    // If the number of pins to be sampled divides the shift register size, we
+    // can use the full SR and FIFO width, and push when the input shift count
+    // exactly reaches 32. If not, we have to push earlier, so we use the FIFO
+    // a little less efficiently.
+    const uint SHIFT_REG_WIDTH = 32;
+    return SHIFT_REG_WIDTH - (SHIFT_REG_WIDTH % pin_count);
+}
+
+void logic_analyser_init(PIO pio, uint sm, uint pin_base, uint pin_count, float div) {
+    // Load a program to capture n pins. This is just a single `in pins, n`
+    // instruction with a wrap.
+    uint16_t capture_prog_instr = pio_encode_in(pio_pins, pin_count);
+    struct pio_program capture_prog = {
+            .instructions = &capture_prog_instr,
+            .length = 1,
+            .origin = -1
+    };
+    uint offset = pio_add_program(pio, &capture_prog);
+
+    // Configure state machine to loop over this `in` instruction forever,
+    // with autopush enabled.
+    pio_sm_config c = pio_get_default_sm_config();
+    sm_config_set_in_pins(&c, pin_base);
+    sm_config_set_wrap(&c, offset, offset);
+    sm_config_set_clkdiv(&c, div);
+    // Note that we may push at a < 32 bit threshold if pin_count does not
+    // divide 32. We are using shift-to-right, so the sample data ends up
+    // left-justified in the FIFO in this case, with some zeroes at the LSBs.
+    sm_config_set_in_shift(&c, true, true, bits_packed_per_word(pin_count));
+    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_RX);
+    pio_sm_init(pio, sm, offset, &c);
+}
+
+void logic_analyser_arm(PIO pio, uint sm, uint dma_chan, uint32_t *capture_buf, size_t capture_size_words,
+                        uint trigger_pin, bool trigger_level) {
+    pio_sm_set_enabled(pio, sm, false);
+    // Need to clear _input shift counter_, as well as FIFO, because there may be
+    // partial ISR contents left over from a previous run. sm_restart does this.
+    pio_sm_clear_fifos(pio, sm);
+    pio_sm_restart(pio, sm);
+
+    dma_channel_config c = dma_channel_get_default_config(dma_chan);
+    channel_config_set_read_increment(&c, false);
+    channel_config_set_write_increment(&c, true);
+    channel_config_set_dreq(&c, pio_get_dreq(pio, sm, false));
+
+    dma_channel_configure(dma_chan, &c,
+        capture_buf,        // Destination pointer
+        &pio->rxf[sm],      // Source pointer
+        capture_size_words, // Number of transfers
+        true                // Start immediately
+    );
+
+    pio_sm_exec(pio, sm, pio_encode_wait_gpio(trigger_level, trigger_pin));
+    pio_sm_set_enabled(pio, sm, true);
+}
+
+void print_capture_buf(const uint32_t *buf, uint pin_base, uint pin_count, uint32_t n_samples) {
+    // Display the capture buffer in text form, like this:
+    // 00: __--__--__--__--__--__--
+    // 01: ____----____----____----
+    uart_puts(UART_ID, "Capture:\n");
+    // Each FIFO record may be only partially filled with bits, depending on
+    // whether pin_count is a factor of 32.
+    uint record_size_bits = bits_packed_per_word(pin_count);
+    for (int pin = 0; pin < pin_count; ++pin) {
+        // uart_puts(UART_ID, "%02d: ", pin + pin_base);
+        uart_puts(UART_ID, "todo");
+        for (int sample = 0; sample < n_samples; ++sample) {
+            uint bit_index = pin + sample * pin_count;
+            uint word_index = bit_index / record_size_bits;
+            // Data is left-justified in each FIFO entry, hence the (32 - record_size_bits) offset
+            uint word_mask = 1u << (bit_index % record_size_bits + 32 - record_size_bits);
+            uart_puts(UART_ID, buf[word_index] & word_mask ? "-" : "_");
+        }
+        uart_puts(UART_ID, "\n");
+    }
+}
+
+
+
+
+
+
 int main() {
 
     // Initialize stdio
     stdio_init_all();
 
-    printf("Initialising VGA...\n");
+    printf("Initialising VGA...");
 
 
     uart_init(UART_ID, BAUD_RATE);
@@ -63,15 +153,35 @@ int main() {
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 
-    uart_puts(UART_ID, "\n Hello, UART!\n");
-
-
     // Initialize the VGA screen
     initVGA() ;
 
-    uart_puts(UART_ID, "\n Hello, UART!\n");
+    uart_puts(UART_ID, "\nPress SPACE to play/pause animation.\n");
+    uart_puts(UART_ID, "Press 'c' to capture.\n");
 
 
+    // We're going to capture into a u32 buffer, for best DMA efficiency. Need
+    // to be careful of rounding in case the number of pins being sampled
+    // isn't a power of 2.
+    uint total_sample_bits = CAPTURE_N_SAMPLES * CAPTURE_PIN_COUNT;
+    total_sample_bits += bits_packed_per_word(CAPTURE_PIN_COUNT) - 1;
+    uint buf_size_words = total_sample_bits / bits_packed_per_word(CAPTURE_PIN_COUNT);
+    uint32_t *capture_buf = malloc(buf_size_words * sizeof(uint32_t));
+    hard_assert(capture_buf);
+
+    // Grant high bus priority to the DMA, so it can shove the processors out
+    // of the way. This should only be needed if you are pushing things up to
+    // >16bits/clk here, i.e. if you need to saturate the bus completely.
+    
+    // todo - uncomment this next line  64CAPTURE_PIN_BASE
+    // bus_ctrl_hw->priority = BUSCTRL_BUS_PRIORITY_DMA_W_BITS | BUSCTRL_BUS_PRIORITY_DMA_R_BITS;
+
+    PIO pio = pio0;
+    uint sm = 3;
+    uint dma_chan = dma_claim_unused_channel(true);
+
+    // logic_analyser_init(pio, sm, CAPTURE_PIN_BASE, CAPTURE_PIN_COUNT, 125000000 / (115200 * 4) /*271.267*/);
+    logic_analyser_init(pio, sm, CAPTURE_PIN_BASE, CAPTURE_PIN_COUNT, 5 * 64 /*271.267*/);
 
     // animation pause
     bool pause = false;
@@ -220,7 +330,25 @@ int main() {
             }
             if ((char)ch == ' ') {
                 pause = !pause; // toggle pause
+            } else if ((char)ch == 'c') {
+
+                printf("Arming trigger (Ctrl-C to exit)\n");
+                logic_analyser_arm(pio, sm, dma_chan, capture_buf, buf_size_words, CAPTURE_PIN_BASE, false);
+
+                // each bit on a uart travels at 115200 bits per second
+                // the clock goes at 125,000,000 hz (I think)
+
+                // so if we want to take, say, 4 samples per bit then we need to sample at  4 * 115200 = 460800 bps
+                // 125,000,000 / 460,800 = 271.267
+
+                // The logic analyser should have started capturing as soon as it saw the
+                // first transition. Wait until the last sample comes in from the DMA.
+                dma_channel_wait_for_finish_blocking(dma_chan);
+
+                print_capture_buf(capture_buf, CAPTURE_PIN_BASE, CAPTURE_PIN_COUNT, CAPTURE_N_SAMPLES);
+                        /* code */
             }
+            
         }
         
 
