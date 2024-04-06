1d436879a581b73abfe7b5840dc7ea8a3d2d7ce5
Peter Stansfeld <pstansfeld@redcreations.co.uk>
2024-04-06 21:38:47 +0100

    Tidy hsync3.pio

--- vga_graphics_demo.c
+++ vga_graphics_demo.c
@@ -71,7 +71,7 @@ const uint CAPTURE_PIN_BASE = HSYNC2; // 16 = hsync, 17 = vsync // 22 = hsync2
 const uint CAPTURE_PIN_COUNT = 4;
 const uint CAPTURE_TRIGGER_PIN = VSYNC; // 8 = hsync, 9 = vsync // 22 = hsync2, 23 = vsync2 NB IGNORED FOR NOW!
 const uint CAPTURE_N_SAMPLES = SCREEN_WIDTH * 96; // enough for 48 screen width's worth of data
-const uint CAPTURE_SAMPLE_FREQ_DIVISOR = 5 * 1; /*271.267*/ // was 5 * 4
+const uint CAPTURE_SAMPLE_FREQ_DIVISOR = 8 * 5 * 1; /*271.267*/ // was 5 * 4
 
 uint g_sample_frequency = CAPTURE_SAMPLE_FREQ_DIVISOR;
 uint8_t g_no_of_captured_pins = CAPTURE_PIN_COUNT;
