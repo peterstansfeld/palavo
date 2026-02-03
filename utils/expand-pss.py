cd20c79aa50c8140c5a1c6723c754acb20039233
Peter Stansfeld <pstansfeld@redcreations.co.uk>
2026-02-03 11:19:49 +0000

    Add ability to expand downloaded DVI framebuffers

--- utils/expand-pss.py
+++ utils/expand-pss.py
@@ -1,4 +1,4 @@
-# Expands a palavo screenshot (its VGA framebuffer) as a 24-bit colour 
+# Expands a palavo VGA framebuffer as a 24-bit colour 
 # 640 x 480 file, which can be converted to a much smaller `.png` file 
 # using the following `imagemagick` command:
 
@@ -28,7 +28,7 @@ if os.path.isfile(src_filename) == False:
     show_help()
     sys.exit(2)
 
-# Read the  input file.
+# Read the input file.
 f = open(src_filename, "rb")
 src_contents = f.read()
 f.close()
@@ -36,24 +36,28 @@ f.close()
 # Expand
 print("Expanding `" + src_filename + "` and saving as `" + dest_filename + "` ...")
 
-length = (4 + (640 / 8)) * 480
+length_vga = int(4 + (640 / 8))
+length_vga = length_vga * 480
 
-if len(src_contents) != (length):
-    print(src_filename + " needs to be " + str(length) + " bytes in length.")
+bytes_per_line_dvi = int((640 * 4) / 5)
+length_dvi = bytes_per_line_dvi * 480
+
+src_len = len(src_contents)
+
+if (src_len!= length_vga) and (src_len != (length_dvi)):
+    print("The file " + src_filename + " needs to be either " + str(length_vga) + " bytes in length for VGA or " + str(length_dvi) + " for DVI.")
     sys.exit(3)
+
+length = length_vga
+
+if src_len == length_dvi:
+    length = length_dvi
 
 # Global variables.
 
-fore_col = 0
-back_col = 0
+# bytearray to hold all the 8-bit RGB values for the output file.
+dest_contents = bytearray(640 * 480 * 3)
 
-fore_red = 0
-fore_green = 0
-fore_blue= 0
-
-back_red = 0
-back_green = 0
-back_blue= 0
 
 def expand_2_bit_color(two_bits):
     r = 0
@@ -67,54 +71,116 @@ def expand_2_bit_color(two_bits):
         # r = r + 85
     return r
 
-# bytearray to hold all the 8-bit RGB values for the output file.
-dest_contents = bytearray(640 * 480 * 3)
 
-# dest_contents index
-dc_i = 0
+def expand_vga_framebuffer():
+    fore_col = 0
+    back_col = 0
 
-# src_contents index
-src_i = 0
+    fore_red = 0
+    fore_green = 0
+    fore_blue= 0
 
-while (src_i < length):
+    back_red = 0
+    back_green = 0
+    back_blue= 0
 
-    # The first four bytes are a little-endian uint32_t consisting of
-    # foreground and background colours and a constant (639). 
-    u16 = (src_contents[src_i + 3] << 8 ) | src_contents[src_i + 2]
+    # dest_contents index
+    dc_i = 0
 
-    back_col = (u16 & 0x3f)
-    fore_col = ((u16 >> 6) & 0x3f)
+    # src_contents index
+    src_i = 0
 
-    # Expand the 2-bit colours into 8-bit ones.
-    fore_red = expand_2_bit_color(fore_col >> 4)
-    fore_green = expand_2_bit_color(fore_col >> 2)
-    fore_blue = expand_2_bit_color(fore_col) 
+    while (src_i < length):
 
-    back_red = expand_2_bit_color(back_col >> 4)
-    back_green = expand_2_bit_color(back_col >> 2)
-    back_blue = expand_2_bit_color(back_col) 
+        # The first four bytes are a little-endian uint32_t consisting of
+        # foreground and background colours and a constant (639). 
+        u16 = (src_contents[src_i + 3] << 8 ) | src_contents[src_i + 2]
 
-    # Skip the line colours etc.
-    src_i += 4
+        back_col = (u16 & 0x3f)
+        fore_col = ((u16 >> 6) & 0x3f)
 
-    for j in range(0, 80):
+        # Expand the 2-bit colours into 8-bit ones.
+        fore_red = expand_2_bit_color(fore_col >> 4)
+        fore_green = expand_2_bit_color(fore_col >> 2)
+        fore_blue = expand_2_bit_color(fore_col) 
 
-        # Expand each bit in the byte rgb values.
-        byte = src_contents[src_i]
-        for j in range(8):
-            if (byte & (1 << j)):
-                dest_contents[dc_i] = fore_red
-                dest_contents[dc_i + 1] = fore_green
-                dest_contents[dc_i + 2] = fore_blue
-            else:
-                dest_contents[dc_i] = back_red
-                dest_contents[dc_i + 1] = back_green
-                dest_contents[dc_i + 2] = back_blue
+        back_red = expand_2_bit_color(back_col >> 4)
+        back_green = expand_2_bit_color(back_col >> 2)
+        back_blue = expand_2_bit_color(back_col) 
+
+        # Skip the line colours etc.
+        src_i += 4
+
+        for j in range(0, 80):
+
+            # expand each bit in the byte rgb values
+            byte = src_contents[src_i]
+            for j in range(8):
+                if (byte & (1 << j)):
+                    dest_contents[dc_i] = fore_red
+                    dest_contents[dc_i + 1] = fore_green
+                    dest_contents[dc_i + 2] = fore_blue
+                else:
+                    dest_contents[dc_i] = back_red
+                    dest_contents[dc_i + 1] = back_green
+                    dest_contents[dc_i + 2] = back_blue
+
+                # next 24-bit RGB value
+                dc_i += 3
+
+            # next byte
+            src_i += 1
+
+
+def expand_dvi_framebuffer():
+
+    fore_col = 0
+    back_col = 0
+
+    fore_red = 0
+    fore_green = 0
+    fore_blue= 0
+
+    back_red = 0
+    back_green = 0
+    back_blue= 0
+
+    # dest_contents index
+    dc_i = 0
+
+    # src_contents index
+    src_i = 0
+
+    while (src_i < length):
+        # read four bytes as a little-endian 32-bit word and trim the two unused bits
+        u32 = ((src_contents[src_i + 3] << 24 ) | (src_contents[src_i + 2] << 16) | (src_contents[src_i + 1] << 8) | src_contents[src_i + 0]) >> 2
+        
+        for j in range(0, 5):
+
+            fore_blue = expand_2_bit_color(u32 & 0b11)
+            u32 >>= 2
+            fore_green = expand_2_bit_color(u32 & 0b11)
+            u32 >>= 2
+            fore_red = expand_2_bit_color(u32 & 0b11)
+            u32 >>= 2
+
+            dest_contents[dc_i] = fore_red
+            dest_contents[dc_i + 1] = fore_green
+            dest_contents[dc_i + 2] = fore_blue
+
+            # next 24-bit RGB value
             dc_i += 3
 
-        # next byte
-        src_i += 1
+        # next u32 word
+        src_i += 4
 
+
+if src_len == length_vga:
+    expand_vga_framebuffer()
+else:
+    expand_dvi_framebuffer()
+
+# Write the output file
 f = open(dest_filename, "wb")
 f.write(dest_contents)
 f.close()
